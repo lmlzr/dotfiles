37b171570f5086abc655c9b76b2b48512743d711
Leon Melzer <leon.2004.me@gmail.com>
2026-07-24 09:59:30 +0200

    i dont use sway at work lol

--- sway/.config/sway/dynamic-wp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd ~/wallpaper/dynamic/wallpaper/
-while true; do
-    PID=`pidof swaybg`
-    swaybg -i ./img/$(date -u -d @$((($(date -u +%s) / 600) * 600)) "+%Hh%M").png -m fill &
-    sleep 1
-    kill $PID
-    sleep 599
-done
